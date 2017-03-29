bba4cef543fe9a498b94032941116b371d94b960
Gowrish.Mallipattana <dbgsm0@c4b301b857f3.hq.target.com>
2017-03-29 13:05:05 +0530

    updates

--- rest-jaxrs-ws-ecl-mvn/ComGmRestJaxrsExamples/src/main/java/com/gm/rest/jaxrs/examples/JaxRsExample.java
+++ rest-jaxrs-ws-ecl-mvn/ComGmRestJaxrsExamples/src/main/java/com/gm/rest/jaxrs/examples/JaxRsExample.java
@@ -2,33 +2,38 @@ package com.gm.rest.jaxrs.examples;
 
 import java.io.*;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response;
 
 import javax.servlet.http.HttpServletResponse;
 
 @Path("/jaxrsexample")
 public class JaxRsExample {
+
+	// @Context
+	// private HttpServletResponse response;
 	
-	//@Context 
-	//private HttpServletResponse response;
-	
+    @Context
+    UriInfo uriInfo;
+
 	@GET
 	@Path("/get1")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String get1() {
 		return ("In Get1 - this ignores all input, produces really no output except this message");
 	}
-	
+
 	@GET
 	@Path("/get2/{id}")
 	@Produces(MediaType.TEXT_PLAIN)
@@ -43,34 +48,52 @@ public class JaxRsExample {
 	public String get3(@QueryParam("id") int id, @QueryParam("name") String name) {
 		return ("In Get3 - input QueryParam is " + id + "-" + name);
 	}
-	
-	
+
 	@GET
 	@Path("/get4")
 	@Produces(MediaType.TEXT_HTML)
 	// Usage /get4?id=10&name=somename
-	public String get4(@Context HttpServletResponse response, @QueryParam("id") int id, @QueryParam("name") String name) {
-			
+	public String get4(@Context HttpServletResponse response, @QueryParam("id") int id,
+			@QueryParam("name") String name) {
+
 		response.setContentType("text/html");
 		response.setHeader("Refresh", "300");
-	
-        return ("<b>In Get4 - Http response with header " + id + " - " + name);
+
+		return ("<b>In Get4 - Http response with header " + id + " - " + name);
 	}
-	
+
 	@GET
 	@Path("/get5")
 	@Produces(MediaType.TEXT_HTML)
 	// Usage /get4?id=10&name=somename
-	public void get5(@Context HttpServletResponse response, @QueryParam("id") int id, @QueryParam("name") String name) 
-	throws Exception {
-			
+	public void get5(@Context HttpServletResponse response, @QueryParam("id") int id, @QueryParam("name") String name)
+			throws Exception {
+
 		response.setContentType("text/html");
 		response.setHeader("Refresh", "300");
-		
+
 		PrintWriter responseBody = response.getWriter();
-		responseBody.print ("<b>In Get5 - Http response with header and body " + id + " - " + name);
+		responseBody.print("<b>In Get5 - Http response with header and body " + id + " - " + name);
 		responseBody.close();
+
+		// return ("<b>In Get5 - Http response with header and body " + id + " -
+		// " + name);
+	}
+
+	@Path("/get6")
+	@GET
+	public String get6(@Context UriInfo uriInfo) {
+
+		// Client used this URI to reach this resource method
+		return ("In get6 - showing URI of this request using UriInfo - " + uriInfo.getAbsolutePath().toASCIIString());
+	}
 	
-        //return ("<b>In Get5 - Http response with header and body " + id + " - " + name);
-	}	
+	@Path("/get7")
+	@GET
+	public String get7() {
+
+		// Client used this URI to reach this resource method
+		return ("In get7 - showing URI of this request using UriInfo - " + uriInfo.getAbsolutePath().toASCIIString());
+	}
+	
 }

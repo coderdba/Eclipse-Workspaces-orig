f57e172bf1635f75199ca3567a550b7e368ff949
Your Name <you@example.com>
2017-04-01 11:29:55 +0530

    updates

--- rest-jaxrs-ws-ecl-mvn/ComGmRestJaxrsExamples/src/main/java/com/gm/rest/jaxrs/examples/JaxRsExample.java
+++ rest-jaxrs-ws-ecl-mvn/ComGmRestJaxrsExamples/src/main/java/com/gm/rest/jaxrs/examples/JaxRsExample.java
@@ -11,6 +11,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.BeanParam;
@@ -28,9 +29,9 @@ public class JaxRsExample {
 
 	// @Context
 	// private HttpServletResponse response;
-	
-    @Context
-    UriInfo uriInfo;
+
+	@Context
+	UriInfo uriInfo;
 
 	@GET
 	@Path("/get1")
@@ -92,7 +93,7 @@ public class JaxRsExample {
 		// Client used this URI to reach this resource method
 		return ("In get6 - showing URI of this request using UriInfo - " + uriInfo.getAbsolutePath().toASCIIString());
 	}
-	
+
 	@Path("/get7")
 	@GET
 	public String get7() {
@@ -100,61 +101,70 @@ public class JaxRsExample {
 		// Client used this URI to reach this resource method
 		return ("In get7 - showing URI of this request using UriInfo - " + uriInfo.getAbsolutePath().toASCIIString());
 	}
-	
+
 	@Path("/get8")
 	@GET
 	public String get8() {
 		// builds a path adding relative uri to the method-URI's absolute path
 		UriBuilder ub = uriInfo.getAbsolutePathBuilder();
 		URI newUri = ub.path("addedThis").build();
-		
+
 		return ("In get8 - showing URI of this request with appended path - " + newUri.toASCIIString());
-	}	
-	
+	}
+
 	@Path("/get9")
 	@GET
 	@Produces("text/plain")
 	public String get9() {
 		// extracts query-params from the URL and displays the keys (not values)
-	    StringBuilder buf = new StringBuilder();
-	    
-	    buf.append("Parameter Names: \n");
-	    
-	    for (String param: uriInfo.getQueryParameters().keySet()) {
-	        buf.append(param);
-	        buf.append("\n");
-	    }
-	    
-	    return buf.toString();
-	}	
+		StringBuilder buf = new StringBuilder();
+
+		buf.append("Parameter Names: \n");
+
+		for (String param : uriInfo.getQueryParameters().keySet()) {
+			buf.append(param);
+			buf.append("\n");
+		}
+
+		return buf.toString();
+	}
+
+	@GET
+	@Path("/get10/{id}")
+	public Response get10(@PathParam("id") int id) {
+
+		Date expirationDate = new Date(System.currentTimeMillis() + 3000); // 3
+																			// secs
+		// return ("In Get10 - input PathParam is " + id);
+
+		// set an expiration in the header of the response
+		return Response.status(Response.Status.OK).entity("Id: " + id).expires(expirationDate).build();
+	}
+
+	@GET
+	@Path("/get11/{pathparam1}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String get11(@BeanParam ParamBean beanParam, String entity) {
+		// System.out.println ("In get11 - pathParam - " +
+		// beanParam.getPathParam());
+		return ("In get11 - " + beanParam.getPathParam());
+	}
+
 	
-    @GET
-    @Path("/get10/{id}")
-    public Response get10(@PathParam("id") int id) {
-    	
-    	Date expirationDate =  new Date(System.currentTimeMillis() + 3000); // 3 secs
-    	//return ("In Get10 - input PathParam is " + id);
-    	
-    	// set an expiration in the header of the response     
-        return Response.status(Response.Status.OK)
-                .entity("Id: " + id )
-                .expires(expirationDate)
-                .build();                    
-    }
-    
-    @GET
-    @Path("/get11/{pathparam1}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String get11(@BeanParam ParamBean beanParam, String entity) {
-    	//System.out.println ("In get11 - pathParam - " + beanParam.getPathParam());
-    	return ("In get11 - " + beanParam.getPathParam() );
-    }
-    
-    /*
-    @GET
-    @Path("/get12")
-    public Viewable get12() {
-    	return new Viewable ("index.html", "something in it");
-    }
-    */
+	@POST
+	@Path("/post1")
+	//@Consumes("application/x­www­form­urlencoded") // not needed
+	@Produces(MediaType.TEXT_HTML)
+	public String post1(@FormParam("firstName") String firstName, @FormParam("lastName") String lastName) {
+		
+		return ("Your name is - " + firstName + " " + lastName);
+	}
+	
+	
+	/*
+	 * @GET
+	 * 
+	 * @Path("/get99") public Viewable get99() { return new Viewable
+	 * ("index.html", "something in it"); }
+	 */
 }

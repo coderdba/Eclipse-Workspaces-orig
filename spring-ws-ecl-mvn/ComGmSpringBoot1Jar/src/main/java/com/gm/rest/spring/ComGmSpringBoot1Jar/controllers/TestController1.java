ced6012c8552c4ffd48ad06cb75da039d6293ba3
Your Name <you@example.com>
2018-06-29 21:07:01 +0530

    updates

--- spring-ws-ecl-mvn/ComGmSpringBoot1Jar/src/main/java/com/gm/rest/spring/ComGmSpringBoot1Jar/controllers/TestController1.java
+++ spring-ws-ecl-mvn/ComGmSpringBoot1Jar/src/main/java/com/gm/rest/spring/ComGmSpringBoot1Jar/controllers/TestController1.java
@@ -33,8 +33,8 @@ public class TestController1 {
     response.sendRedirect("https://www.google.com/search?q=" + thing);
   }
   
-  @GetMapping("/mycookie/{cookiename}")
-   void mycookie(@PathVariable String cookiename, HttpServletRequest request, HttpServletResponse response) throws IOException {
+  @GetMapping("/mycookie/{myCookieName}")
+   void mycookie(@PathVariable String myCookieName, HttpServletRequest request, HttpServletResponse response) throws IOException {
     
 	// set up http session - https://www.journaldev.com/1907/java-session-management-servlet-httpsession-url-rewriting
 	HttpSession session = request.getSession();
@@ -43,7 +43,8 @@ public class TestController1 {
 	session.setMaxInactiveInterval(10); // or (30*60) for 30 min
 	  
 	// create cookies
-    Cookie aCookie = new Cookie("aCookieName",cookiename);
+    Cookie aCookie = new Cookie("aCookieName",myCookieName);
+    aCookie.setMaxAge(10); //seconds
     
     //http://www.java2s.com/Code/Java/Servlets/Usecookietosavesessiondata.htm
     //Cookie sessionIdCookie = new Cookie("sessionId", generateSessionId());
@@ -67,9 +68,11 @@ public class TestController1 {
     response.getWriter().write("A cookie name is - " + aCookie.getName() + ", value is - " + aCookie.getValue() + "<br>" + 
     							"Session Id Cookie name is - " + 
     							sessionIdCookie.getName() + ", value is - " + sessionIdCookie.getValue()
-	+ "Session Id Cookie expiry seconds is - " + 
-	 sessionIdCookie.getMaxAge());	   			
-    								
+	+ "<br>" + "Session Id Cookie expiry seconds is - " + 
+	 sessionIdCookie.getMaxAge()
+	+ "<br>" + "A Cookie expiry seconds is - " + aCookie.getMaxAge())
+    ;	   			
+    		 
     response.getWriter().flush();
     response.getWriter().close();
     

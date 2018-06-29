2cca14b3c0c1df9157d3e8358cd187e8d6bac8fb
Your Name <you@example.com>
2018-06-29 20:31:45 +0530

    updates

--- spring-ws-ecl-mvn/ComGmSpringBoot1Jar/src/main/java/com/gm/rest/spring/ComGmSpringBoot1Jar/controllers/TestController1.java
+++ spring-ws-ecl-mvn/ComGmSpringBoot1Jar/src/main/java/com/gm/rest/spring/ComGmSpringBoot1Jar/controllers/TestController1.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //servlet response related
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Cookie;
 import java.net.URLEncoder;
 
+//exceptions
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
@@ -31,14 +34,28 @@ public class TestController1 {
   }
   
   @GetMapping("/mycookie/{cookiename}")
-   void mycookie(@PathVariable String cookiename, HttpServletResponse response) throws IOException {
+   void mycookie(@PathVariable String cookiename, HttpServletRequest request, HttpServletResponse response) throws IOException {
     
+	// http session - https://www.journaldev.com/1907/java-session-management-servlet-httpsession-url-rewriting
+	HttpSession session = request.getSession();
+	session.setAttribute("user", "myselfTheUser");
+	//setting session to expiry in N sec
+	session.setMaxInactiveInterval(10); // or (30*60) for 30 min
+	
+	// more code from - https://www.journaldev.com/1907/java-session-management-servlet-httpsession-url-rewriting
+	//Cookie userName = new Cookie("user", user);
+	//userName.setMaxAge(30*60);
+	//response.addCookie(userName);
+	//response.sendRedirect("LoginSuccess.jsp");
+	  
 	// create cookies
     Cookie aCookie = new Cookie("aCookieName",cookiename);
     
     //http://www.java2s.com/Code/Java/Servlets/Usecookietosavesessiondata.htm
-    Cookie sessionIdCookie = new Cookie("sessionId", generateSessionId());
+    //Cookie sessionIdCookie = new Cookie("sessionId", generateSessionId());
     
+    Cookie sessionIdCookie = new Cookie("sessionId", request.getSession().getId());
+    sessionIdCookie.setMaxAge(10); //seconds
     
     // content type
     response.setContentType("text/html");
@@ -55,7 +72,9 @@ public class TestController1 {
     //response.getWriter().write("A cookie is - " + cookiename);
     response.getWriter().write("A cookie name is - " + aCookie.getName() + ", value is - " + aCookie.getValue() + "<br>" + 
     							"Session Id Cookie name is - " + 
-    							sessionIdCookie.getName() + ", value is - " + sessionIdCookie.getValue());	
+    							sessionIdCookie.getName() + ", value is - " + sessionIdCookie.getValue()
+	+ "Session Id Cookie expiry seconds is - " + 
+	 sessionIdCookie.getMaxAge());	   			
     								
     response.getWriter().flush();
     response.getWriter().close();

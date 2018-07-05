564390e8b15544ad83e6e49af9703f2687c996d5
Your Name <you@example.com>
2018-07-05 22:28:34 +0530

    updates

---
+++ spring-ws-ecl-mvn/comGmMusic/src/main/java/com/gm/music/NotesList.java
@@ -0,0 +1,39 @@
+package com.gm.music;
+
+
+public class NotesList {
+	
+	private NotesList() {} // prevent instantiation
+	
+	/* Notes frequencies reference
+	 261 // c4 sa 
+	 277 // c#4 r1
+	 329 // e g3
+	 349 // f m1
+	 392 // g p
+	 415 // g# d1
+	 494 // b n3
+	 523 // c5 s
+	 */
+	
+	/*
+	public static final float s2 = 261f;
+	public static final float r21 = 277f;
+	public static final float g23 = 329f;
+	public static final float m21 = 349f;
+	public static final float p2 = 392f;
+	public static final float d21 = 415f;
+	public static final float n23 = 494f;
+	public static final float s3 = 523f;
+	*/
+	
+	public static final float s2 = 261f;
+	public static final float r21 = 277f;
+	public static final float g23 = 329f;
+	public static final float m21 = 349f;
+	public static final float p2 = 392f;
+	public static final float d21 = 415f;
+	public static final float n23 = 494f;
+	public static final float s3 = 523f;
+
+}
--- spring-ws-ecl-mvn/comGmMusic/src/main/java/com/gm/music/Tone1Play.java
+++ spring-ws-ecl-mvn/comGmMusic/src/main/java/com/gm/music/Tone1Play.java
@@ -1,7 +1,17 @@
 package com.gm.music;
 
+import com.gm.music.NotesList;
 
 public class Tone1Play {
+	
+	static float s2  = NotesList.s2;
+	static float r21 = NotesList.r21;
+	static float g23 = NotesList.g23;
+	static float m21 = NotesList.m21;
+	static float p2  = NotesList.p2;
+	static float d21 = NotesList.d21;
+	static float n23 = NotesList.n23;
+	static float s3  = NotesList.s3;
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -11,23 +21,28 @@ public class Tone1Play {
 		
 		//myTone.playOneNote(100, 150);
 		
-		 /*
-		 createTone(261, 100); // c4 sa 
-		 createTone(277, 100); // c#4 ri1
-		 createTone(329, 100); // e g3
-		 createTone(349, 100); // f m1
-		 createTone(392, 100); // g p
-		 createTone(415, 100); // g# d1
-		 createTone(494, 100); // b n3
-		 createTone(523, 100); // c5 s
-		 */
-		
 		int[] songLine1 = {261,277,329,349,392,415,494,523};
 		//myTone.playNoteListOneSecond(songLine1, 100);
-		myTone.playNoteList(songLine1, 100, 0.5f);
+		//myTone.playNoteList(songLine1, 100, 0.5f);
 		
-		float[][] songLine2 = { {261f, 0.5f}, {277f, 0.5f}, {329f, 0.5f}, {349f, 0.5f} };
-		myTone.playNoteList(songLine2, 100);
+		//float[][] songLine2 = { {261f, 0.5f}, {277f, 0.5f}, {329f, 0.5f}, {349f, 0.5f} };
+		//myTone.playNoteList(songLine2, 100);
+		
+		//float[][] songLine3 = { {s2, 0.5f}, {r21, 0.5f}, {g23, 0.5f}, {m21, 0.5f}, {p2, 0.5f}, 
+									//{d21, 0.5f}, {n23, 0.5f}, {s3, 0.5f}};
+		
+		//myTone.playNoteList(songLine3, 100);
+		
+		float[][] songLine4 = { 
+				 {s2, 0.5f}, {r21, 0.5f}, {m21, 1.0f}, {g23, 0.5f}, {r21, 0.5f},
+				{s2, 0.5f}, {r21, 0.5f}, {g23, 0.5f}, {r21, 0.5f}, {s2, 1.0f},
+				{r21, 0.5f}, {m21, 0.5f}, {p2, 0.5f}, {d21, 0.5f}, {m21, 0.5f}, {p2, 0.5f},
+				{d21, 0.5f}, {p2, 0.5f}, {m21, 0.5f}, {g23, 0.5f}, {r21, 0.5f}, {s2, 0.5f},
+				{s2, 0.5f}, {r21, 0.5f}, {m21, 1.0f}, {g23, 0.5f}, {r21, 0.5f}
+				,{s2, 0.5f}, {r21, 0.5f}, {g23, 0.5f}, {r21, 0.5f}, {s2, 1.0f}};
+
+        myTone.playNoteList(songLine4, 100);
+		
 	}
 
 

7aaedc17e184353cf058b4a4f09af48443b072ed
Your Name <you@example.com>
2018-07-06 22:15:29 +0530

    updates

--- spring-ws-ecl-mvn/comGmMusic/src/main/java/com/gm/music/ReadSongFileAndPrint.java
+++ spring-ws-ecl-mvn/comGmMusic/src/main/java/com/gm/music/ReadSongFileAndPrint.java
@@ -10,12 +10,32 @@ public class ReadSongFileAndPrint {
 		
 		List<String> elements = songFileReader.getElements();
 		int elementsCount = elements.size();
+		String note;
+		int  length;
 		
-		System.out.println(elements.toString());
+		System.out.println("List of elements " + elements.toString());
 		
 		for (int i=0; i<elementsCount; i++) {
 			
-			// note, octave, length
+			// note-octave, length
+			
+			String element = elements.get(i);
+			
+			System.out.println("Element " + element);
+			
+			String[] splitString = element.split("\\.");
+						
+			note = splitString[0];
+			
+			if (splitString.length > 1) {
+				
+			    length = Integer.parseInt(splitString[1]);
+			}
+			else {
+				length = 1; 
+			}
+			
+			System.out.println(element + " " + note + " " + length);
 			
 		}
 		

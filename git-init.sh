cee48d5d720d08086efa97094c074e0334a696a2
coderdba <coderdba@coder.com>
2018-07-26 18:53:24 +0530

    updates

--- git-init.sh
+++ git-init.sh
@@ -1,3 +1,6 @@
+git config user.email coderdba@coder.com
+git config user.name coderdba
+
 echo "# Eclipse-Workspaces" >> README.md
 git init
 #git add README.md
@@ -5,3 +8,11 @@ git add .
 git commit -m "first commit"
 git remote add origin https://github.com/coderdba/Eclipse-Workspaces.git
 git push -u origin master
+
+echo
+echo
+echo
+echo RUN IT AGAIN IF IT GIVES SOME INITIAL ERROR
+echo
+echo
+echo

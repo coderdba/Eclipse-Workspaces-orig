db946b04f91b5ff21ec98323f3749930fff67221
coderdba <coderdba@coder.com>
2018-07-27 22:34:04 +0530

    updates

--- spring-ws-ecl-mvn/ComGmSpringBootJDBC/src/main/java/com/gm/springboot/jdbc/oracle/model/Customer.java
+++ spring-ws-ecl-mvn/ComGmSpringBootJDBC/src/main/java/com/gm/springboot/jdbc/oracle/model/Customer.java
@@ -22,4 +22,10 @@ public class Customer {
     }
 
     //getters and setters and toString...
+    public void setAll(int id, String name, String email, Date date) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.date = date;
+    }
 }

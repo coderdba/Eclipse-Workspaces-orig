7d6f3ee146f3b08d5f9209666c18e8a642da554b
coderdba <coderdba@coder.com>
2018-08-02 17:41:35 +0530

    updates

---
+++ kafka/kafka-binary-file-publish-consume/src/main/java/com/gm/kafka/binfile/Config.java
@@ -0,0 +1,39 @@
+package com.gm.kafka.binfile;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Config {
+
+	// read configuration from config file
+	HashMap<String, String> conf;
+	
+	public Config(String filePath) throws FileNotFoundException, IOException
+	{
+	conf=new HashMap();
+	File file=new File(filePath);
+	BufferedReader br=new BufferedReader(new FileReader(file));
+	String[] vals;
+	String line=br.readLine();
+	
+	while (line!=null)
+	{
+	if (!line.startsWith("#"))
+	{
+	vals=line.toLowerCase().split("=");
+	conf.put(vals[0], vals[1]);
+	}
+	line=br.readLine();
+	}
+	 
+	}
+	 
+	public String get(String key)
+	{
+	return conf.get(key.toLowerCase());
+	}	
+}

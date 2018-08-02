ed601f1ca7f13fc94e60642fdfa58bd8446d7761
coderdba <coderdba@coder.com>
2018-08-02 19:32:05 +0530

    updates

--- kafka-run-jar.sh
+++ kafka-run-jar.sh
@@ -1,8 +1,18 @@
+if [ $# lt 1 ]
+then
+
+echo USAGE:  $0 your_lan_username
+exit
+
+fi
+
+myusername=$1
+
 # Example
-#java -cp "/Users/dbgsm0/opt/kafka_2.10-0.10.0.0/libs/*":"./kafka-simple-producer/target/kafka-simple-producer-0.0.1-SNAPSHOT.jar" com.gm.kafka.producer.simple.SimpleProducer topic2
+#java -cp "/Users/${myusername}/opt/kafka_2.10-0.10.0.0/libs/*":"./kafka-simple-producer/target/kafka-simple-producer-0.0.1-SNAPSHOT.jar" com.gm.kafka.producer.simple.SimpleProducer topic2
 
 # actual ones
-#java -cp "/Users/dbgsm0/opt/kafka_2.10-0.10.0.0/libs/*":"./kafka-simple-producer/target/kafka-simple-producer-0.0.1-SNAPSHOT.jar" com.gm.kafka.producer.simple.SimpleProducer topic2
+#java -cp "/Users/${myusername}/opt/kafka_2.10-0.10.0.0/libs/*":"./kafka-simple-producer/target/kafka-simple-producer-0.0.1-SNAPSHOT.jar" com.gm.kafka.producer.simple.SimpleProducer topic2
 
-java -cp "/Users/dbgsm0/opt/kafka_2.10-0.10.0.0/libs/*":"kafka/kafka-binary-file-publish-consume/target/kafka-binary-file-publish-consume-0.0.1-SNAPSHOT.jar" com.gm.kafka.binfile.BinaryProducer kafka.binary.properties
+java -cp "/Users/${myusername}/opt/kafka_2.10-0.10.0.0/libs/*":"kafka/kafka-binary-file-publish-consume/target/kafka-binary-file-publish-consume-0.0.1-SNAPSHOT.jar" com.gm.kafka.binfile.BinaryProducer kafka.binary.properties
 

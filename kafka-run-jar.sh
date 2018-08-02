c1904fc97ab0ac799b02829c246b8c970f0fa8e3
coderdba <coderdba@coder.com>
2018-08-02 19:30:37 +0530

    updates

---
+++ kafka-run-jar.sh
@@ -0,0 +1,8 @@
+# Example
+#java -cp "/Users/dbgsm0/opt/kafka_2.10-0.10.0.0/libs/*":"./kafka-simple-producer/target/kafka-simple-producer-0.0.1-SNAPSHOT.jar" com.gm.kafka.producer.simple.SimpleProducer topic2
+
+# actual ones
+#java -cp "/Users/dbgsm0/opt/kafka_2.10-0.10.0.0/libs/*":"./kafka-simple-producer/target/kafka-simple-producer-0.0.1-SNAPSHOT.jar" com.gm.kafka.producer.simple.SimpleProducer topic2
+
+java -cp "/Users/dbgsm0/opt/kafka_2.10-0.10.0.0/libs/*":"kafka/kafka-binary-file-publish-consume/target/kafka-binary-file-publish-consume-0.0.1-SNAPSHOT.jar" com.gm.kafka.binfile.BinaryProducer kafka.binary.properties
+
